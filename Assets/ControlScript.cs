239eab04397dac55e39de92fc5320353d29d4400
suosong <suosong@sohu.com>
2015-09-02 18:39:02 +0800

    完善了镜头跟随、使用了动画状态机

--- Assets/ControlScript.cs
+++ Assets/ControlScript.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class ControlScript : MonoBehaviour {
 
+    private float hold_time = 0; // 被持球时长
+    private GameObject holder = null;    // 持球人
+    public GameObject player1 = null;
+    public GameObject player2 = null;
+
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        // 无持球人或持球时间超过0.3秒情况下，距离球小于0.2米且是与球距离最近的人获得球
+
 	    //float s = Input.GetAxis("Horizontal");
         if (Input.GetKey(KeyCode.W))
         {
